12d638a1f4a999751af9de1cfa12db82d78b2e79
Yunhao <yunhi.0799@gmail.com>
2024-02-04 15:43:57 +0100

    Hilt dependency injection

---
+++ FakeNewDetectorApp/app/src/main/java/com/yunhao/fakenewsdetector/FakeNewsDetectorApplication.kt
@@ -0,0 +1,8 @@
+package com.yunhao.fakenewsdetector
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FakeNewsDetectorApplication : Application() {
+}
