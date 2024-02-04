a860506e5361b8f6050232c0e2d8574827cfa8ce
Yunhao <yunhi.0799@gmail.com>
2024-02-04 14:56:28 +0100

    Initial commit

---
+++ app/src/main/java/com/yunhao/fakenewsdetector/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.yunhao.fakenewsdetector
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import com.yunhao.fakenewsdetector.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var _appBarConfiguration: AppBarConfiguration
+    private lateinit var _binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+
+        setSupportActionBar(_binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        _appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, _appBarConfiguration)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(_appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+}
