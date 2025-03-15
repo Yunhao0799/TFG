7071c7847984f02a8ebcc29c0fb0b2559f0219a8
Yunhao <yunhi.0799@gmail.com>
2025-03-15 19:18:03 +0100

    Use ngrok rerouting

--- FakeNewDetectorApp/app/src/main/java/com/yunhao/fakenewsdetector/data/network/ApiClient.kt
+++ FakeNewDetectorApp/app/src/main/java/com/yunhao/fakenewsdetector/data/network/ApiClient.kt
@@ -7,7 +7,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    private const val BASE_URL = "http://10.0.2.2:8000/"
+    // private const val BASE_URL = "http://10.0.2.2:8000/" // only for emulator
+    private const val BASE_URL = "https://sharing-cool-sole.ngrok-free.app/"
 
     val instance by lazy {
         val csrfInterceptor = Interceptor { chain ->
