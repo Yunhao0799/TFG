3c3dba4ab56c2560137ed860664a657feac2d95c
Yunhao0799 <60287763+Yunhao0799@users.noreply.github.com>
2025-04-14 15:38:31 +0100

    Add logout feature using a toolbar (#15)
    * add appbar
    
    * Open popup and hide status
    
    * improve popup
    
    * change icon colors
    
    * WIP on feature/TAS-35
    
    * frotend logout

--- FakeNewsDetectorApp/app/src/main/java/com/yunhao/fakenewsdetector/ui/view/HomeFragment.kt
+++ FakeNewsDetectorApp/app/src/main/java/com/yunhao/fakenewsdetector/ui/view/HomeFragment.kt
@@ -1,23 +1,28 @@
 package com.yunhao.fakenewsdetector.ui.view
 
+import android.content.Context
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupWindow
+import androidx.core.view.ViewCompat
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.library.baseAdapters.BR
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.MutableLiveData
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.transition.MaterialFadeThrough
 import com.yunhao.fakenewsdetector.R
 import com.yunhao.fakenewsdetector.databinding.FragmentHomeBinding
+import com.yunhao.fakenewsdetector.databinding.PopupProfileMenuBinding
 import com.yunhao.fakenewsdetector.ui.view.adapters.ChatAdapter
-import com.yunhao.fakenewsdetector.ui.view.adapters.data.ChatMessage
 import com.yunhao.fakenewsdetector.ui.view.common.FragmentBase
 import com.yunhao.fakenewsdetector.ui.viewmodel.HomeViewModel
 import com.yunhao.fakenewsdetector.ui.viewmodel.common.ViewModelBase
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.Dispatchers
 
 @AndroidEntryPoint
 class HomeFragment : FragmentBase<FragmentHomeBinding, ViewModelBase>() {
@@ -25,6 +30,7 @@ class HomeFragment : FragmentBase<FragmentHomeBinding, ViewModelBase>() {
 
     private lateinit var adapter: ChatAdapter
 
+    private val isProfilePopupOpen = MutableLiveData(false)
     override fun getLayoutId(): Int {
         return R.layout.fragment_home
     }
@@ -51,6 +57,10 @@ class HomeFragment : FragmentBase<FragmentHomeBinding, ViewModelBase>() {
 
 
     override fun setUpListeners() {
+        isProfilePopupOpen.observe(viewLifecycleOwner) {
+            (requireActivity() as StartActivity).isPopupOpen.postValue(it)
+        }
+
         binding?.let { b ->
             b.recyclerView.viewTreeObserver.addOnGlobalLayoutListener {
                 val lastPosition = adapter.itemCount - 1
@@ -59,6 +69,20 @@ class HomeFragment : FragmentBase<FragmentHomeBinding, ViewModelBase>() {
                         b.recyclerView.smoothScrollToPosition(lastPosition)
                     }
                 }, 100)
+            }
+
+            b.appBar.let {
+                it.setOnMenuItemClickListener { menuItem ->
+                    when (menuItem.itemId) {
+                        R.id.profile -> {
+
+                            openProfilePopup(it as View)
+                            true
+                        }
+
+                        else -> false
+                    }
+                }
             }
         }
     }
@@ -74,5 +98,38 @@ class HomeFragment : FragmentBase<FragmentHomeBinding, ViewModelBase>() {
                 }
             }
         }
+    }
+
+    fun openProfilePopup(anchorView: View) {
+        val binding = PopupProfileMenuBinding.inflate(LayoutInflater.from(context), null, false)
+
+        val popupWindow = PopupWindow(
+            binding.root,
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            true
+        ).apply {
+            isOutsideTouchable = true
+            elevation = 20f
+            // setBackgroundDrawable(ContextCompat.getDrawable(requireContext(), android.R.color.transparent))
+            setOnDismissListener { isProfilePopupOpen.postValue(false) }
+        }
+
+        binding.apply {
+            logoutRowBackground.setOnClickListener {
+                popupWindow.dismiss()
+
+                // TODO: call viewmodel to remove token from DB
+
+                findNavController().navigate(R.id.action_homeFragment_to_LoginFragment)
+            }
+        }
+
+        // Support RTL layout
+        val isRtl = ViewCompat.getLayoutDirection(anchorView) == ViewCompat.LAYOUT_DIRECTION_RTL
+        val offsetX = if (isRtl) 200 else -200
+
+        popupWindow.showAsDropDown(anchorView, offsetX, 10, Gravity.END)
+        isProfilePopupOpen.postValue(true)
     }
 }
