89b362f06bf3516c84b5f49c7295e3e68c29b4d9
Yunhao <yunhi.0799@gmail.com>
2025-05-02 00:40:44 +0200

    Add favorites in backend

--- FakeNewsDetectorBackend/djangoBackend/djangoProject/newsCache/serializers.py
+++ FakeNewsDetectorBackend/djangoBackend/djangoProject/newsCache/serializers.py
@@ -1,6 +1,21 @@
 from rest_framework import serializers
 
+from newsCache.models import FavoriteNews, NewsArticle
 
-class NewsResponseSerializer(serializers.Serializer):
-    source = serializers.CharField()
-    data = serializers.JSONField()
+
+class NewsArticleSerializer(serializers.ModelSerializer):
+    source = serializers.SerializerMethodField()
+    is_favorite = serializers.SerializerMethodField()
+
+    class Meta:
+        model = NewsArticle
+        fields = ['id', 'title', 'description', 'url', 'urlToImage', 'published_at', 'source', 'is_favorite']
+
+    def get_source(self, obj):
+        return {'name': obj.source_name}
+
+    def get_is_favorite(self, obj):
+        user = self.context.get('request').user
+        if not user or not user.is_authenticated:
+            return False
+        return FavoriteNews.objects.filter(user=user, article=obj).exists()
--- FakeNewsDetectorBackend/djangoBackend/djangoProject/newsCache/urls.py
+++ FakeNewsDetectorBackend/djangoBackend/djangoProject/newsCache/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import NewsView
+from .views import NewsView, toggle_favorite, list_favorites
 
 urlpatterns = [
     path('', NewsView.as_view(), name='news-view'),
+    path('favorite-toggle/', toggle_favorite, name='toggle-favorite'),
+    path('favorites/', list_favorites, name='list-favorites'),
 ]
